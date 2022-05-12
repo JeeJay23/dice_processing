b4eefd1794c45d059c56d3a959f7092649c14399
Jader <jvjader@gmail.com>
2022-05-12 16:52:48 +0200

    initial label works

--- dices.py
+++ dices.py
@@ -1,4 +1,3 @@
-from random import gauss
 import matplotlib.pyplot as plt
 from matplotlib import image
 
@@ -6,34 +5,56 @@ import numpy as np
 
 from skimage.color import rgb2gray
 from skimage.filters import threshold_otsu, gaussian
+from skimage.segmentation import clear_border
+from skimage.measure import label, regionprops
+from skimage.morphology import closing, disk, opening
 
-original = image.imread("img\\dice2.jpg")
-print(original.shape)
+from os import listdir
+from os.path import isfile,  join
 
-# convert to grayscale
-gray = rgb2gray(original)
+def processImg(img):
+    # convert to grayscale
+    gray = rgb2gray(img)
 
-# add blur to remove noise
-gray = gaussian(gray, 4)
+    # add blur to remove noise
+    gray = gaussian(gray, 4)
 
-# convert to int32
-gray = gray * 255
-gray = gray.astype(np.int32)
+    # convert to int32
+    gray = gray * 255
+    gray = gray.astype(np.int32)
 
-# treshold an binarize
-tresh = threshold_otsu(gray)
-binary = gray > tresh
+    # treshold an binarize
+    tresh = threshold_otsu(gray)
+    binary = gray > tresh
+    binary = opening(binary, disk(4))
+    binary = clear_border(binary)
 
-# plot
-fig, axs = plt.subplots(ncols=3, nrows=2, figsize=(10,8), sharex='row', sharey='row')
+    labeled, amount = label(binary, return_num=True)
 
-axs[0,0].imshow(original)
+    # plot
+    fig, axs = plt.subplots(ncols=4, nrows=1, figsize=(10,8), sharex='row', sharey='row')
 
-axs[0,1].imshow(gray, cmap='gray', vmin=0, vmax=255)
+    axs[0].imshow(img)
+    axs[0].set_title("original")
+    axs[0].axis('off')
 
-axs[1,0].hist(gray.ravel(), bins=256)
-axs[1,0].axvline(tresh, color='r')
+    axs[1].imshow(gray, cmap='gray', vmin=0, vmax=255)
+    axs[1].set_title("grayscale + gauss")
+    axs[1].axis('off')
 
-axs[0,2].imshow(binary, cmap='gray', vmin=0, vmax=1)
+    axs[2].imshow(binary, cmap='gray', vmin=0, vmax=1)
+    axs[2].set_title("tresholded")
+    axs[2].axis('off')
+
+    axs[3].imshow(labeled, cmap='plasma', vmin=0, vmax=amount)
+    axs[3].set_title("labeled")
+    axs[3].annotate("N dice: %i" % amount, xy=[0,500], xytext=[0,500], color='w')
+    axs[3].axis('off')
+
+
+imgPath = "img\\"
+images = [image.imread(join(imgPath,f)) for f in listdir(imgPath) if isfile(join(imgPath, f))]
+for i in images:
+    processImg(i)
 
 plt.show()
